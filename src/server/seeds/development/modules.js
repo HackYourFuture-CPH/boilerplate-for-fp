bcdc8067d39e3175d3af612a9e6fba1a8b20abd4
Toni Petrina <tonipetrina@hotmail.com>
2021-01-16 21:29:10 +0100

    Code clean

--- src/server/seeds/development/modules.js
+++ src/server/seeds/development/modules.js
@@ -1,10 +1,10 @@
 // TODO: This is a sample seed file for demonstration. Remove before adding real ones.
 
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex('modules')
     .del()
-    .then(function() {
+    .then(function () {
       // Inserts seed entries
       return knex('modules').insert([
         {
