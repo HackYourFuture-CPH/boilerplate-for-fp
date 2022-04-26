75b1ff7ec3c4f5c81d97d941f4f67d844b9fff9b
Zaki Wasik <zakiwa@gmail.com>
2022-04-26 19:49:23 +0200

    Add linting to react, eslint hierarchy

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,8 +1,8 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log('problem');
   return (
     <div className="App">
       <header className="App-header">
