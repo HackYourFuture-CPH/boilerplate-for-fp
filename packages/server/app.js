dcafa438b7880173f351e418014aaaaea5e440c6
dpfernandes <daniel.pastor.fernandes@gmail.com>
2022-06-14 00:57:25 +0200

    add cors to dependencies

--- packages/server/app.js
+++ packages/server/app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const cors = require('cors');
 const router = require('./api/routes/index');
 
 const app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
