d5383a05c41be990cbd1e606473825693dfe256b
Daniel Fernandes <daniel.pastor.fernandes@gmail.com>
2022-06-15 11:40:35 +0200

    fixing 404 route
    fixing 404 route

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/*" element={<PageNotFound />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </Router>
     </div>
